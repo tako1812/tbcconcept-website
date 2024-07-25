972891468cbf6a347292d25b46186a474feea7d5
tamar robakidze <tamar.robakidze@mfg.ge>
2024-07-25 13:49:21 +0400

    navition dropdown feature and functionality

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
 "strict mode";
-
-const sectionHero = document.querySelector(".section-hero");
-const header = document.querySelector(".header");
-
-const stickyNav = function (entries) {
-  const [entry] = entries;
-  console.log(entry);
-
-  if (entry.isIntersecting) {
-    header.classlist.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-};
-const headerObserver = new IntersectionObserver(stickyNav, {
-  root: null,
-  threshold: 0,
-});
-headerObserver.observe(header);
-/***************************************/
-/************************************ */
-/****************************************/
-/*************************************** */
 //slider
 const slide = document.querySelectorAll(".slide");
 const slider = document.querySelector(".slider");
@@ -34,3 +11,22 @@ slider.style.transform = "scale(0.4)translateX(-100px)";*/
 const sliderCountainer = document.querySelector(".slider-countainer");
 const slideFeature = document.querySelectorAll(".slide-feature-second");
 slideFeature.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
+/***************************************/
+/* NAV Dropdown */
+/****************************************/
+
+const dropdownBtn = document.querySelectorAll(".dropdown-btn");
+const dropdownIcon = document.querySelector(".dropdown-icon");
+//
+dropdownBtn.forEach(function (btn) {
+  console.log(btn);
+  btn.addEventListener("click", function () {
+    dropdownIcon.classList.toggle("dropdown-icon-rotate");
+    const dropdownContent = btn.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+});
