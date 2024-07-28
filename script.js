5b2254fcfd4d4607473c5dde07ca1b7d1be92c9c
tako <tamar.robakidze@mfg.ge>
2024-07-28 23:04:45 +0400

    work on slider functionality and navigation features

--- script.js
+++ script.js
@@ -37,156 +37,14 @@ dropdownBtnNav.forEach(function (btn) {
 ///////////////////////////////////////
 // Slider
 /*
-const slider = function () {
-  const slider = document.querySelector(".slider");
-  const slides = document.querySelectorAll(".slide");
 
-  let curSlide = 0;
-  const maxSlide = slides.length;
-  let isDragStart = false;
-  let prevPageX;
-  let prevScrollLeft;
-
-  // Functions
-
-  const goToSlide = function (slide) {
-    slides.forEach(
-      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
-    );
-  };
-
-  // Next slide
-  const nextSlide = function () {
-    if (curSlide === maxSlide - 1) {
-      curSlide = 0;
-    } else {
-      curSlide++;
-    }
-
-    goToSlide(curSlide);
-    activateDot(curSlide);
-  };
-
-  const prevSlide = function () {
-    if (curSlide === 0) {
-      curSlide = maxSlide - 1;
-    } else {
-      curSlide--;
-    }
-    goToSlide(curSlide);
-    activateDot(curSlide);
-  };
-
-  const init = function () {
-    goToSlide(0);
-  };
-  init();
-
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") prevSlide();
-    e.key === "ArrowRight" && nextSlide();
-  });
-
-  let isDragStart = false;
-  let prevPageX;
-  let prevScrollLeft;
-
-  const dragStart = (e) => {
-    //updating global variables value on mouse down event
-    isDragStart = true;
-    prevPageX = e.pageX;
-    prevScrollLeft = slider.scrollLeft;
-  };
-
-  const dragging = (e) => {
-    //scrolling images/carousel to left according to mouse pointer
-    if (!isDragStart) return;
-    e.preventDefault();
-    let positionDiff = e.pageX - prevPageX;
-    slider.scrollLeft = prevScrollLeft - positionDiff;
-  };
-
-  const dragStop = () => {
-    isDragStart = false;
-  };
-  slider.addEventListener("mousedown", dragStart);
-  slider.addEventListener("mousemove", dragging);
-  slider.addEventListener("mouseup", dragStop);
-};
-slider();
-*/
-/*
-const slider = document.querySelector(".slider");
-const slides = document.querySelectorAll(".slide");
-
-let isDown = false;
-let startX;
-let scrollLeft;
-
-slider.addEventListener("mousedown", (e) => {
-  e.preventDefault();
-  isDown = true;
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-  //console.log(startX);
-});
-
-slider.addEventListener("mouseleave", (e) => {
-  e.preventDefault();
-  isDown = false;
-});
-
-slider.addEventListener("mouseup", (e) => {
-  e.preventDefault();
-  isDown = false;
-});
-
-slider.addEventListener("mousemove", (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = x - startX;
-  slider.scrollLeft = scrollLeft - walk;
-});*/
 /************************************************* */
 /************************************************* */
 /************************************************* */
 /************************************************** */
 /************************************************** */
 /*
-const awardsCountainer = document.querySelector(".awards-countainer");
-awardsCountainer;
 
-let isDown = false;
-let startX;
-let scrollLeft;
-
-awardsCountainer.addEventListener("mousedown", (e) => {
-  e.preventDefault();
-  isDown = true;
-  startX = e.pageX - awardsCountainer.offsetLeft;
-  scrollLeft = awardsCountainer.scrollLeft;
-  //console.log(startX);
-});
-
-awardsCountainer.addEventListener("mouseleave", (e) => {
-  e.preventDefault();
-  isDown = false;
-});
-
-awardsCountainer.addEventListener("mouseup", (e) => {
-  e.preventDefault();
-  isDown = false;
-});
-
-awardsCountainer.addEventListener("mousemove", (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - awardsCountainer.offsetLeft;
-  const walk = x - startX;
-  awardsCountainer.scrollLeft = scrollLeft - walk;
-});
-*/
 /////////////////////////////////////////////
 /////// SLIDER functionality
 /************************************************** */
@@ -200,7 +58,6 @@ const sliderFunctionality = function (slider) {
   let isDown = false;
   let startX;
   let scrollLeft;
-
   const dragStart = (e) => {
     e.preventDefault();
     isDown = true;
@@ -229,10 +86,34 @@ const sliderFunctionality = function (slider) {
   slider.addEventListener("mouseup", dragStop);
   slider.addEventListener("mouseleave", dragleave);
 };
-console.log(sliderFunctionality(awardsCountainer));
-console.log(sliderFunctionality(slider));
-console.log(sliderFunctionality(productsSlider));
-console.log(sliderFunctionality(packageSlider));
+sliderFunctionality(awardsCountainer);
+sliderFunctionality(slider);
+sliderFunctionality(productsSlider);
+sliderFunctionality(packageSlider);
+
+const slidOffers = (direction) => {
+  if (direction === "left") {
+    slider.scrollLeft -= 300;
+  } else {
+    slider.scrollLeft += 300;
+  }
+};
+
+const slidProducts = (direction) => {
+  if (direction === "left") {
+    productsSlider.scrollLeft -= 300;
+  } else {
+    productsSlider.scrollLeft += 300;
+  }
+};
+
+const slidAwards = (direction) => {
+  if (direction === "left") {
+    awardsCountainer.scrollLeft -= 300;
+  } else {
+    awardsCountainer.scrollLeft += 300;
+  }
+};
 
 /*//
 /********** */
